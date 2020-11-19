0bd4a9b0b903ce26a060d77435a1d42e60787cfe
lartpang <lartpang@163.com>
2020-11-19 12:19:58 +0800

    delete some unuseful files.

--- backbone/deeplabv3_origin/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2020/8/5
-# @Author  : Lart Pang
-# @Email   : lartpang@163.com
-# @File    : __init__.py.py
-# @Project : OpticalFlowBasedVOS
-# @GitHub  : https://github.com/lartpang
